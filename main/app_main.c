7b53fca1ee92bb3557a6b3245725405aa249f8d3
fernandoeqc <fernandoeqc@live.com>
2022-08-30 01:06:22 -0300

    cleaning

--- main/app_main.c
+++ main/app_main.c
@@ -38,28 +38,10 @@ static void vTaskJson(void *arg);
 static void vTaskMqtt(void *arg);
 
 void app_main(void)
-{
-/*     
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_TCP", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_SSL", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
-
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
+{  
     ESP_ERROR_CHECK(example_connect());
 
     mqtt_app_start();
-*/
 
     createTasks();
     for(;;)
